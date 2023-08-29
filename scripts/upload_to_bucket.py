2347f176cfc52bce07c98696eecd987d3962eee2
yekta <ngyekta@gmail.com>
2023-08-30 02:01:20 +0300

    known file

--- scripts/upload_to_bucket.py
+++ scripts/upload_to_bucket.py
@@ -3,41 +3,40 @@ import time
 import argparse
 from azure.storage.blob import BlobServiceClient
 
-FILE_EXTENSIONS = [".safetensors", ".json"]
+FILE_EXTENSIONS = [".safetensors", ".anotherextension"]
 
 
 def upload_files(directory, blob_service_client, bucket, known_files):
+    container_client = blob_service_client.get_container_client(bucket)
+    existing_blobs = {blob.name for blob in container_client.list_blobs()}
+
     for root, _, files in os.walk(directory):
         for file in files:
             if not any(file.endswith(ext) for ext in FILE_EXTENSIONS):
                 continue
 
             file_path = os.path.join(root, file)
-            file_stat = os.stat(file_path)
-            file_key = (file_path, file_stat.st_size, file_stat.st_mtime)
-
-            if file_key in known_files:
-                continue
-
             blob_name = os.path.basename(file_path)
-            blob_client = blob_service_client.get_blob_client(
-                container=bucket, blob=blob_name
-            )
 
-            if blob_client.exists():
+            if blob_name in existing_blobs:
                 print(f"{blob_name} already exists in Azure Blob. Skipping.")
+                known_files.add(blob_name)
                 continue
 
+            if blob_name in known_files:
+                continue
+
+            blob_client = container_client.get_blob_client(blob_name)
             with open(file_path, "rb") as f:
                 blob_client.upload_blob(f)
 
             print(f"Uploaded {blob_name}")
-            known_files.add(file_key)
+            known_files.add(blob_name)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description=f"Watch a directory and upload new files with specified extensions to Azure Blob Storage."
+        description="Watch a directory and upload new files to Azure Blob Storage."
     )
     parser.add_argument(
         "-c", "--connection-string", required=True, help="Azure connection string."
