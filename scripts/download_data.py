a5275bb036a82c6aaf3e49481c2c6e31c14d62e5
yekta <ngyekta@gmail.com>
2023-08-29 07:07:23 +0300

    fix script

--- scripts/download_data.py
+++ scripts/download_data.py
@@ -43,7 +43,6 @@ def main():
     # Create the parent and download folders if they don't exist
     os.makedirs(download_folder, exist_ok=True)
 
-    # Get blob clients for each container
     # Get container clients for each container
     caption_container_client = blob_service_client.get_container_client(
         caption_container_name
@@ -58,36 +57,34 @@ def main():
         with open(download_path, "wb") as f:
             f.write(blob_data.readall())
 
-    # Get all the blobs (files) in the caption container
-    caption_blobs = caption_container_client.list_blobs()
-
-    # Loop through each JSON blob in the caption container
-    for blob in caption_blobs:
-        blob_client = caption_container_client.get_blob_client(blob)
+    # Iterate over the caption blobs
+    for blob_info in caption_container_client.list_blobs():
+        blob_client = caption_container_client.get_blob_client(blob_info)
         json_str = blob_client.download_blob().readall().decode("utf-8")
         json_data = json.loads(json_str)
 
-        # Extract text and corresponding image name
-        caption_text = json_data["result"][0]["value"]["text"][0]
-        image_name = os.path.basename(json_data["task"]["data"]["captioning"])
+        # Get the image file name from the JSON blob
+        captioning_data = json_data["task"]["data"]["captioning"]
+        image_name = captioning_data.split("/")[-1]
+        base_name, _ = os.path.splitext(image_name)
 
-        # Determine the actual image name in the training-data bucket (JPEG or PNG)
-        for ext in [".jpeg", ".jpg", ".png"]:
+        # Search for the image blob in the training data container with possible extensions
+        for ext in ["png", "jpg", "jpeg"]:
             image_blob_client = training_data_container_client.get_blob_client(
-                image_name.replace(".jpeg", ext)
+                f"{base_name}.{ext}"
             )
+
             if image_blob_client.exists():
-                actual_image_name = image_name.replace(".jpeg", ext)
+                # Download the image
+                download_blob(
+                    image_blob_client,
+                    os.path.join(download_folder, f"{base_name}.{ext}"),
+                )
                 break
 
-        # Download the image
-        download_path = os.path.join(download_folder, actual_image_name)
-        download_blob(image_blob_client, download_path)
-
-        # Save the caption text to a .txt file
-        txt_name = f"{os.path.splitext(actual_image_name)[0]}.txt"
-        txt_path = os.path.join(download_folder, txt_name)
-        with open(txt_path, "w") as f:
+        # Extract the caption text and write it to a .txt file
+        caption_text = json_data["result"][0]["value"]["text"][0]
+        with open(os.path.join(download_folder, f"{base_name}.txt"), "w") as f:
             f.write(caption_text)
 
     print("Download and processing completed.")
